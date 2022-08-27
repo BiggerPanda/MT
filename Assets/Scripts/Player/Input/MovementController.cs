2dd98dc8270c804495648af69a78b2fd7446aadb
Jakub Michalski <jakub@pixelstorm.pl>
2022-08-27 23:32:40 +0200

    give up on stairs control

--- Assets/Scripts/Player/Input/MovementController.cs
+++ Assets/Scripts/Player/Input/MovementController.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
+public enum MovementState
+{
+    Walking,  // 0
+    Running,  // 1
+    Crouching, // 2
+    Stairs, // 3
+    Falling, // 4
 
+}
 
 public class MovementController : Controller
 {
+    [ReadOnly]
+    public MovementState movementState;
+
     [Header("Movement")]
     [SerializeField] private float _walkSpeed = 5f;
     [SerializeField] private float _runSpeed = 11f;
@@ -16,30 +28,47 @@ public class MovementController : Controller
     [SerializeField] private float _height = 2f;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private bool grounded;
-    
+    [Header("Slope Handle")]
+    [SerializeField] private float _slopeLimit = 45f;
+    [SerializeField] private float _stepOffset = 0.5f;
+    [Header("Stairs")]
+    [SerializeField] private float _starirsMinDepth = 0.3f;
+    [SerializeField] private float _maxStepHeight = 0.3f;
+
     private Vector2 _movementInput;
     private Vector3 _movementDirection;
     private float _movementSpeed;
     private bool _isSprinting, _isCrouching;
+    private RaycastHit _slopeHit;
+    private RaycastHit _stairsHit;
+
+    private bool _exitSlope;
+
     public override void ReadInput(Vector2 input)
     {
         _movementInput = input;
     }
-#region Movement
+    #region Movement
     public void Jump()
     {
-        if(!grounded)
+        _exitSlope = true;
+        if (!grounded)
             return;
         _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
-        _rigidbody.AddForce(transform.up * _jumpForce* _rigidbody.mass, ForceMode.Impulse);
+        _rigidbody.AddForce(transform.up * _jumpForce * _rigidbody.mass, ForceMode.Impulse);
+        Invoke("ResetJump", 0.1f);
+    }
+    private void ResetJump()
+    {
+        _exitSlope = false;
     }
 
     public void Crouch()
     {
         _isCrouching = !_isCrouching;
-        if(_isCrouching)
+        if (_isCrouching)
         {
-            transform.localScale = new Vector3(transform.localScale.x, 0.5f , transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, 0.5f, transform.localScale.z);
             _movementSpeed = _crouchSpeed;
         }
         else
@@ -51,7 +80,7 @@ public class MovementController : Controller
     }
 
     public void Sprint()
-    {   
+    {
         _isSprinting = !_isSprinting;
         _movementSpeed = _isSprinting ? _runSpeed : _walkSpeed;
     }
@@ -59,19 +88,40 @@ public class MovementController : Controller
     private void Movement()
     {
         _movementDirection = transform.forward * _movementInput.y + transform.right * _movementInput.x;
-        if(grounded) 
-           _rigidbody.AddForce(_movementDirection * _movementSpeed *_rigidbody.mass * 10f , ForceMode.Force);
+        if (IsOnSlope() && !_exitSlope)
+        {
+            _rigidbody.AddForce(SlopeMoveDirection() * _movementSpeed * _rigidbody.mass * 10f, ForceMode.Force);
+            if (_rigidbody.velocity.y > 0f)
+                _rigidbody.AddForce(Vector3.down * _rigidbody.mass * 10f, ForceMode.Force);
+        }
+        else if (grounded)
+            _rigidbody.AddForce(_movementDirection * _movementSpeed * _rigidbody.mass * 10f, ForceMode.Force);
     }
 
     private void GroundCheck()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, _height*0.5f + 0.1f , _groundLayer);
-    } 
-#endregion 
-#region Handle Input
+        grounded = Physics.Raycast(transform.position, Vector3.down, _height * 0.5f + 0.2f, _groundLayer);
+    }
+
+    private bool IsOnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, _height * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+            return angle <= _slopeLimit && angle != 0;
+        }
+        return false;
+    }
+    private Vector3 SlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(_movementDirection, _slopeHit.normal).normalized;
+    }
+
+    #endregion
+    #region Handle Input
     private void DragHangle()
     {
-        if(grounded)
+        if (grounded)
         {
             _rigidbody.drag = _groundDrag;
         }
@@ -82,20 +132,60 @@ public class MovementController : Controller
     }
     private void SpeedHandle()
     {
-        Vector3 velocity = new Vector3(_rigidbody.velocity.x , 0 , _rigidbody.velocity.z);
-        if(velocity.magnitude > _movementSpeed)
+        if (IsOnSlope() && !_exitSlope)
         {
-            _rigidbody.velocity =  new Vector3(velocity.normalized.x * _movementSpeed,
-                                              _rigidbody.velocity.y ,
-                                               velocity.normalized.z * _movementSpeed);
+            if (_rigidbody.velocity.magnitude > _movementSpeed)
+            {
+                _rigidbody.velocity = _rigidbody.velocity.normalized * _movementSpeed;
+            }
+        }
+        else
+        {
+            Vector3 velocity = new Vector3(_rigidbody.velocity.x, 0, _rigidbody.velocity.z);
+            if (velocity.magnitude > _movementSpeed)
+            {
+                _rigidbody.velocity = new Vector3(velocity.normalized.x * _movementSpeed,
+                                                  _rigidbody.velocity.y,
+                                                   velocity.normalized.z * _movementSpeed);
+            }
+        }
+
+    }
+    private void HandleState()
+    {
+        if (_isCrouching)
+        {
+            movementState = MovementState.Crouching;
+        }
+        else if (_isSprinting && grounded)
+        {
+            movementState = MovementState.Running;
+        }
+        else if (grounded || IsOnSlope())
+        {
+            movementState = MovementState.Walking;
+        }
+        else
+        {
+            movementState = MovementState.Falling;
         }
     }
-#endregion
+    private void HandleGravity()
+    {
+        _rigidbody.useGravity = !IsOnSlope();
+    }
+    private void Handler()
+    {
+        DragHangle();
+        SpeedHandle();
+        HandleState();
+        HandleGravity();
+    }
+    #endregion
     private void Update()
     {
         GroundCheck();
-        DragHangle();
-        SpeedHandle();
+        Handler();
     }
 
     private void FixedUpdate()
